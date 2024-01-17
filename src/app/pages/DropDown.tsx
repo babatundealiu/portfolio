c9a256dd7b8bc16fe4668177e07b323e14459973
babatundealiu <debaljconsult@gmail.com>
2024-01-17 14:07:02 +0100

    mobile responsiveness

--- src/app/pages/DropDown.tsx
+++ src/app/pages/DropDown.tsx
@@ -13,45 +13,45 @@ const DropDown:React.FC<IdropDown> = ({props, setShow}) => {
   }
 
   return (
-    <div className='sm:h-[30vh]  sm:w-[100%] bg-slate-200 mt-10 xs:h-[25vh] xs:top-[30px]   sm:text-primary md:text-primary md:w-[200px] top-[45px] md:top-[80px] absolute left-0 right-0 bottom-'>
+    <div className='sm:h-[30vh]  sm:top-[30px] sm:mx-[211px] xs:mx-[168px] bg-slate-200 mt-10 xs:h-[28vh] xs:top-[30px] w-[50%]   sm:text-primary md:text-primary md:w-[200px] top-[45px] md:top-[80px] absolute left-0 right-0 bottom-'>
       {
         props 
      && (
-      <div className='text-center flex-col  text-[#8f8f8e] text-[20px]  sm:ml-4 mt-5 md:ml-7'>
+      <div className='text-center flex-col  text-[#8f8f8e] text-[20px]  sm:ml-4 mt-10 md:ml-7'>
         <Link offset={-70}
             duration={1000}
             activeClass="active"
             smooth={true}  
             to="home">
-        <div onClick={remove} className='sm:p-0 sm:mb-2'>HOME</div>
+        <div onClick={remove} className='sm:p-0 sm:mb-2 xs:p-0 xs:mb-2'>HOME</div>
        </Link>
        <Link offset={-70}
             duration={1000}
             activeClass=""
             smooth={true}
             to="about">
-       <div onClick={remove} className='sm:p-0 sm:mb-2'>ABOUT</div>
+       <div onClick={remove} className='sm:p-0 sm:mb-2 xs:p-0 xs:mb-2'>ABOUT</div>
         </Link>       
        <Link offset={-70}
             duration={1000}
             activeClass="active"
             smooth={true}
             to="skills">
-       <div onClick={remove} className=' sm:p-0 sm:mb-2'>SKILLS</div>
+       <div onClick={remove} className=' sm:p-0 sm:mb-2 xs:p-0 xs:mb-2'>SKILLS</div>
        </Link>
        <Link offset={-70}
             duration={1000}
             activeClass="active"
             smooth={true}
             to="work">
-       <div onClick={remove} className='sm:p-0 sm:mb-2'>WORK</div>
+       <div onClick={remove} className='sm:p-0 sm:mb-2 xs:p-0 xs:mb-2'>WORK</div>
      </Link>
        <Link offset={-70}
             duration={1000}
             activeClass="active"
             smooth={true}
             to="contact">
-       <div onClick={remove} className='sm:p-0 sm:mb-2 '>CONTACT</div>
+       <div onClick={remove} className='sm:p-0 sm:mb-2 xs:p-0 xs:mb-2 '>CONTACT</div>
       </Link>
      </div>
      )
