55427cc7918d5b0e1b2260af0c5eff8c3ba623b2
babatundealiu <debaljconsult@gmail.com>
2024-01-17 13:48:11 +0100

    mobile responsiveness

--- src/app/pages/DropDown.tsx
+++ src/app/pages/DropDown.tsx
@@ -13,11 +13,11 @@ const DropDown:React.FC<IdropDown> = ({props, setShow}) => {
   }
 
   return (
-    <div className='sm:h-[30vh]  sm:w-[100%] bg-slate-200 mt-10 sm:text-primary md:text-primary md:w-[200px] top-[45px] md:top-[80px] absolute left-0 right-0 bottom-'>
+    <div className='sm:h-[30vh]  sm:w-[100%] bg-slate-200 mt-10 xs:h-[25vh] xs:top-[30px]   sm:text-primary md:text-primary md:w-[200px] top-[45px] md:top-[80px] absolute left-0 right-0 bottom-'>
       {
         props 
      && (
-      <div className='text-center flex-col text-[#8f8f8e] text-[20px]  sm:ml-4 mt-5 md:ml-7'>
+      <div className='text-center flex-col  text-[#8f8f8e] text-[20px]  sm:ml-4 mt-5 md:ml-7'>
         <Link offset={-70}
             duration={1000}
             activeClass="active"
