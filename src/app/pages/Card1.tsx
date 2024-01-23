d848e8ef7c4ab96b66ed2d127912a1058f3e79f8
babatundealiu <debaljconsult@gmail.com>
2024-01-23 12:31:12 +0100

    added

--- src/app/pages/Card1.tsx
+++ src/app/pages/Card1.tsx
@@ -12,10 +12,13 @@ const Card1:React.FC<Iprops> = ({image, title, content, linkIcon, Git}) => {
   return (
     <div>
       <div className="transform  duration-700 hover:scale-110 shadow transition-ease-out text-primary  bg-[#201e21] h-[350px] md:h-[280px] w-[300px]  md:w-[200px] rounded">
+        <a href="https://admirable-monstera-0ad7cd.netlify.app/">
         <img 
             className="h-[230px] md:h-[130px] object-fit rounded rounded-b-none "
             src={image}
             alt="" />
+        </a>
+
             <div className="text-slate-300 mt-4 font-extrabold text-[16px] text-center justify-center flex">
                 {title}
             </div>
