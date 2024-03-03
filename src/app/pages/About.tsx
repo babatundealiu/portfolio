ab5a119db84d2381ed585e54e0fd10a615fe483d
babatundealiu <debaljconsult@gmail.com>
2024-03-03 14:10:52 +0100

    new commit

--- src/app/pages/About.tsx
+++ src/app/pages/About.tsx
@@ -12,6 +12,7 @@ const About:React.FC = () => {
       <div className="text-white text-[27px]  xs:text-[17px]  xs:w-[90%] xs:mx-auto sm:text-[16px] justify-center sm:mb-10 sm:mx-auto sm:w-[100%] text-center font-extrabold ">
         Effective <span className='text-secondary'>Software Development</span> Process <br />Make the <span className='text-secondary'>Complex </span>Appear to be <span className='text-secondary'>Simple</span>.
       </div>
+      .
       <div className="flex flex-wrap gap-7 xs:w-[90%] xs:mx-auto sm:flex-col lg:gap-3 lg:m-5  sm:mx-auto sm:w-[86%] mt-7 md:gap-3 justify-center">
         <Card icon={js} title='WEB DEVELOPMENT' content='As a skilled web developer, I excel in crafting stunning and intuitive websites that seamlessly blend cutting-edge design with robust functionality, ensuring an exceptional user experience. My passion for web development drives me to deliver impactful solutions that exceed client expectations and elevate online presence.' />
         <Card icon={pic} title='FRONTEND DEVELOPMENT' content='I am enthusiastic about the opportunity to bring my frontEnd development expertise on board and contribute to the creation of cutting-edge web applications that provide seamless user experiences. I am excited to contribute and engage in projects that continue to shape the future of web development' />
